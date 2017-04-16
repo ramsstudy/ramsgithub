4a6e23943cf9e0b71eb851abb3f524c8f8a187fd
Rams <Rams@RamsLaptopp>
2017-04-17 00:00:37 +0530

    Change-Id: I90130f8ce15d04d847ac6898aa72b1eeea96f0e1

--- javademoproject1/src/javademoproject1/javademopgm1.java
+++ javademoproject1/src/javademoproject1/javademopgm1.java
@@ -3,5 +3,7 @@ package javademoproject1;
 public class javademopgm1 {
 	public static void main(String[] args) {
 	System.out.println ("Output : Hello World"); 
+	System.out.println ("Output : Hello World ***1***");
+	System.out.println ("Output : Hello World ***12***");
 	}
 }
